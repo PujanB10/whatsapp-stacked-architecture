d7433366851253509a128846948bcea31e3a5f84
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-09-24 15:20:33 +0545

    fix: remove unwanted dependency

--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:stacked/stacked.dart';
-import 'package:whatsapp_stacked_architecture/ui/common/ui_helpers.dart';
-
 import 'startup_viewmodel.dart';
 
 class StartupView extends StackedView<StartupViewModel> {
