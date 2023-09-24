e9c5d560db6b933efe43786a3a04b02a92141876
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-09-24 15:23:40 +0545

    feat: create message list view widget library for chat page

---
+++ lib/ui/views/chat_page/widgets/message_list_view_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:whatsapp_stacked_architecture/ui/views/chat_page/chat_page_viewmodel.dart';
+import 'package:whatsapp_stacked_architecture/ui/views/chat_page/widgets/chat_box_widget.dart';
+
+/// A messages viewing widget for building a list of chat messages.
+class MesssageListViewWidget extends StatelessWidget {
+  /// Creates a [MesssageListViewWidget]
+  ///
+  /// [userName] must not be null.
+  const MesssageListViewWidget(
+      {super.key, required this.userName, required this.viewModel});
+
+  /// [userName] is the username with whom the user's messages is to
+  /// be displayed.
+  final String userName;
+  final ChatPageViewModel viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      /// The numbers of messages present at an instance with the given user.
+      itemCount: context.watch<ChatPageViewModel>().dummyChat[userName].length,
+      itemBuilder: ((BuildContext context, int index) => ChatBox(
+          message: context.read<ChatPageViewModel>().dummyChat[userName][index]
+              ["message"],
+          isUser: context.read<ChatPageViewModel>().dummyChat[userName][index]
+              ["isUser"])),
+    );
+  }
+}
