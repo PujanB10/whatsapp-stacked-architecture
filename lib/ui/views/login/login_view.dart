359d7554f7bde563a51a788628cce0c5467eee28
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-09-29 07:24:06 +0545

    feat: refactor login view code to use custom widget

--- lib/ui/views/login/login_view.dart
+++ lib/ui/views/login/login_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:stacked/stacked.dart';
 import 'package:whatsapp_stacked_architecture/ui/common/ui_helpers.dart';
+import 'package:whatsapp_stacked_architecture/ui/views/common_widgets/custom_text_button_widget.dart';
+import 'package:whatsapp_stacked_architecture/ui/views/common_widgets/text_field_widget.dart';
 import 'login_viewmodel.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -25,56 +27,46 @@ class LoginView extends StackedView<LoginViewModel> {
                 style: GoogleFonts.acme(fontSize: 45),
               ),
               verticalSpaceMedium,
-              SizedBox(
+              TextFieldWidget(
+                textEditingController: viewModel.emailController,
                 width: 300,
-                child: TextField(
-                  controller: viewModel.emailController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      prefixIcon: Icon(Icons.person_outline),
-                      hintText: "Email"),
-                ),
+                hintText: "Email",
+                prefixIcon: const Icon(Icons.person_2_outlined),
               ),
               verticalSpaceMedium,
-              SizedBox(
+              TextFieldWidget(
+                isObscure: true,
+                textEditingController: viewModel.passwordController,
                 width: 300,
-                child: TextField(
-                  obscureText: true,
-                  controller: viewModel.passwordController,
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.lock_outline),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      hintText: "Password"),
-                ),
+                hintText: "Password",
+                prefixIcon: const Icon(Icons.lock_outline),
               ),
-              TextButton(onPressed: () {}, child: Text("Forgot password?")),
+              TextButton(
+                  onPressed: () {}, child: const Text("Forgot password?")),
               verticalSpaceSmall,
-              SizedBox(
-                  width: 200,
-                  height: 50,
-                  child:
-                      ElevatedButton(onPressed: () {}, child: Text("Log In"))),
+              CustomTextButtonWidget(
+                onPressed: () {
+                  viewModel.logIn();
+                },
+                textInButton: "Log In",
+              ),
               verticalSpaceMedium,
-              Text("Or Sign Up Using"),
+              const Text("Or Sign Up Using"),
               verticalSpaceSmall,
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   IconButton(
-                      onPressed: () {}, icon: Icon(FontAwesomeIcons.google)),
+                      onPressed: () {},
+                      icon: const Icon(FontAwesomeIcons.google)),
                   IconButton(
-                      onPressed: () {}, icon: Icon(FontAwesomeIcons.facebook)),
+                      onPressed: () {},
+                      icon: const Icon(FontAwesomeIcons.facebook)),
                   IconButton(
-                      onPressed: () {}, icon: Icon(FontAwesomeIcons.instagram))
+                      onPressed: () {},
+                      icon: const Icon(FontAwesomeIcons.instagram))
                 ],
               ),
-
-              // SizedBox(
-              //     height: 50,
-              //     width: 200,
-              //     child: ElevatedButton(onPressed: () {}, child: Text("Sign Up"))),
             ],
           ),
         ),
