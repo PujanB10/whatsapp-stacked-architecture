804aa8f509c3c0ecd5f4494d77ae61183e66ab75
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-09-29 07:15:47 +0545

    feat: create view model for login page

--- lib/ui/views/login/login_viewmodel.dart
+++ lib/ui/views/login/login_viewmodel.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:whatsapp_stacked_architecture/app/app.locator.dart';
+import 'package:whatsapp_stacked_architecture/app/app.router.dart';
+import 'package:whatsapp_stacked_architecture/services/login_service_service.dart';
 
 class LoginViewModel extends BaseViewModel {
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+  final _logInService = locator<LoginServiceService>();
+  final _navigationService = locator<NavigationService>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   get emailController => _emailController;
   get passwordController => _passwordController;
+
+  /// Async function that calls [logIn] service from the
+  /// service class
+  void logIn() async {
+    var res = await _logInService.logIn(
+        emailController.text, passwordController.text);
+    if (res.isNotEmpty) {
+      /// If the service returns not empty string then
+      /// it Sign ups the user and navigates to Home View.
+      _navigationService.navigateToHomeView();
+    }
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 }
