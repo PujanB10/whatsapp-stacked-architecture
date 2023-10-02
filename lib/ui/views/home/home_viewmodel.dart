99cd4e03a966ed9da50850b7c9677bfec684034c
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-02 16:24:43 +0545

    feat: access fetch user list service in viewmodel

--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,71 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:whatsapp_stacked_architecture/app/app.locator.dart';
 import 'package:whatsapp_stacked_architecture/datamodels/user_model.dart';
 import 'package:whatsapp_stacked_architecture/app/app.router.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:whatsapp_stacked_architecture/services/fetch_user_list_service.dart';
 
 class HomeViewModel extends BaseViewModel {
+  List<Users> dummy = [];
   final _navigationService = locator<NavigationService>();
   final _dialogService = locator<DialogService>();
-
-  final List<Users> _dummy = [
-    Users(
-        userName: "Pujan",
-        messages: "Hey there! How's your day going?",
-        time: "2:00 AM",
-        imageURL: "lib/ui/assets/icon1.jpeg"),
-    Users(
-        userName: "Ram",
-        messages: "I just tried a new recipe.",
-        time: "3:00 AM",
-        imageURL: "lib/ui/assets/icon2.png"),
-    Users(
-        userName: "Shyam",
-        messages: "Do you believe in aliens?",
-        time: "6:00 AM",
-        imageURL: "lib/ui/assets/icon1.jpeg"),
-    Users(
-        userName: "Gita",
-        messages: "I can't wait for the weekend to relax.",
-        time: "2:03 PM",
-        imageURL: "lib/ui/assets/icon2.png"),
-    Users(
-        userName: "Sita",
-        messages: "Have you ever been on a trip?",
-        time: "3:20 AM",
-        imageURL: "lib/ui/assets/icon1.jpeg"),
-    Users(
-        userName: "Rakesh",
-        messages: "What's your favorite book of all time?",
-        time: "3:00 AM",
-        imageURL: "lib/ui/assets/icon2.png"),
-    Users(
-        userName: "Abishek",
-        messages: "Just finished a great workout.",
-        time: "6:00 AM",
-        imageURL: "lib/ui/assets/icon1.jpeg"),
-    Users(
-        userName: "Rita",
-        messages: "Thinking about starting a new hobby.",
-        time: "2:50 PM",
-        imageURL: "lib/ui/assets/icon2.png"),
-    Users(
-        userName: "Nita",
-        messages: "The weather today is so unpredictable.",
-        time: "2:03 PM",
-        imageURL: "lib/ui/assets/icon1.jpeg")
-  ];
-
-  List get dummy => _dummy;
-
-  Map<String, String> userInfo(int index) {
-    return {
-      "userName": _dummy[index].userName!,
-      "userMessages": _dummy[index].messages!,
-      "imageURL": _dummy[index].imageURL!,
-      "time": _dummy[index].time!
-    };
-  }
+  final _fetchUserService = locator<FetchUserListService>();
 
   void callChatPage(String userName, String imageUrl) {
     _navigationService.navigateToChatPageView(
@@ -78,5 +23,9 @@ class HomeViewModel extends BaseViewModel {
     var res = await _dialogService.showConfirmationDialog(
         title: "Do you want to exit?");
     return (res!.confirmed);
+  }
+
+  Future<List<Users>> fetchUserList() {
+    return _fetchUserService.fetchUserList();
   }
 }
