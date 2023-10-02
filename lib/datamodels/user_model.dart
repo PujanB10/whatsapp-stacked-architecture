9a8ca8901a72d5a626352d6aef772b14aa2c8a8b
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-02 16:23:24 +0545

    fix: remodel user model for home screen

--- lib/datamodels/user_model.dart
+++ lib/datamodels/user_model.dart
@@ -1,7 +1,18 @@
 class Users {
-  String? userName;
-  String? messages;
-  String? imageURL;
-  String? time;
-  Users({this.userName, this.messages, this.imageURL, this.time});
+  String firstName;
+  String lastName;
+  String email;
+
+  Users({required this.firstName, required this.lastName, required this.email});
+
+  factory Users.fromJson(Map<String, dynamic> json) {
+    return Users(
+        firstName: json["firstName"],
+        lastName: json["lastName"],
+        email: json["email"]);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {"firstName": firstName, "lastName": lastName, "email": email};
+  }
 }
