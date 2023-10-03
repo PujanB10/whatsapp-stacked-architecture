9f588e8c7867f66d078e683993bb02de9ffc338d
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-03 17:06:52 +0545

    fix: remodel chat according to database

--- lib/datamodels/chat_model.dart
+++ lib/datamodels/chat_model.dart
@@ -1,20 +1,26 @@
-class ChatModel {
-  String userName;
-  String? messages;
-  bool? isUser;
-  ChatModel({required this.userName, this.messages, this.isUser});
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-  Map<String, dynamic> toMapNew() {
-    return {
-      userName: {
-        [
-          {"message": messages, "isUser": isUser}
-        ],
-      }
-    };
+class ChatModel {
+  String sentBy;
+  String message;
+  Timestamp sentTime;
+  ChatModel(
+      {required this.message, required this.sentBy, required this.sentTime});
+
+  factory ChatModel.fromJson(
+      QueryDocumentSnapshot<Map<String, dynamic>> snapShot) {
+    final json = snapShot.data();
+    return ChatModel(
+        message: json["message"],
+        sentBy: json["sentBy"],
+        sentTime: json["sentTime"]);
   }
 
-  Map<String, dynamic> toMapExisting() {
-    return {"message": messages, "isUser": isUser};
+  Map<String, dynamic> toJson() {
+    return {
+      "sentBy": sentBy,
+      "message": message,
+      "sentTime": sentTime,
+    };
   }
 }
