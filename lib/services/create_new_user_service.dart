a96d7361bb995f41240ac706de9f2e10cb597b43
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-05 14:37:59 +0545

    fix: add comments and handle errors from Firebase Auth

--- lib/services/create_new_user_service.dart
+++ lib/services/create_new_user_service.dart
@@ -1,15 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 /// A service class to create a new user in Firebase Authentication Service.
 class CreateNewUserService {
   final db = FirebaseFirestore.instance;
-  Future<UserCredential?> createNewUser(String email, String password) async {
+
+  Future<String> createNewUser(String email, String password) async {
     try {
       /// Stores the user credential in the credential variable
       /// that is received through an async function that calls
       /// firebase authentication serevice.
-      final UserCredential credential =
+      UserCredential userCredential =
           await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: email,
         password: password,
@@ -17,26 +19,14 @@ class CreateNewUserService {
 
       /// Returns the credential retreived from Firebase Auth service by converting
       /// it into string.
-      return credential;
+      return "successful:${userCredential.user!.uid}";
     } on FirebaseAuthException catch (e) {
-      /// Check for weak password
-      if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
-
-        /// Check if email already exists.
-      } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
-      }
-    } catch (e) {
-      print(e);
+      return e.code;
     }
-    return null;
-
-    /// returns an empty string if the credential cannot be retrieved.
   }
 
   Future<void> addInDatabase(Map<String, dynamic> user) async {
     db.collection("users").add(user).then((DocumentReference doc) =>
-        print('DocumentSnapshot added with ID: ${doc.id}'));
+        debugPrint('DocumentSnapshot added with ID: ${doc.id}'));
   }
 }
