52c92bfcd425f96e3cb02251b1a51ac150535ae9
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-05 14:53:21 +0545

    feat: add snackbar on register page on register errors

--- lib/ui/views/register/register_view.dart
+++ lib/ui/views/register/register_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:stacked/stacked.dart';
 import 'package:whatsapp_stacked_architecture/ui/common/ui_helpers.dart';
@@ -23,57 +24,62 @@ class RegisterView extends StackedView<RegisterViewModel> {
               verticalSpaceMassive,
               Text(
                 "Sign Up",
-                style: GoogleFonts.acme(fontSize: 45),
+                style: GoogleFonts.acme(fontSize: 50.sp),
               ),
-              verticalSpaceMedium,
+              verticalSpace(0.03.sh),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   TextFieldWidget(
                     textEditingController: viewModel.firstNameController,
-                    width: 150,
+                    width: 0.39.sw,
                     hintText: "First Name",
                     prefixIcon: const Icon(Icons.person_outline),
                   ),
                   horizontalSpaceTiny,
                   TextFieldWidget(
                     textEditingController: viewModel.lastNameController,
-                    width: 150,
+                    width: 0.39.sw,
                     hintText: "Last Name",
                     prefixIcon: const Icon(Icons.person_outline),
                   )
                 ],
               ),
-              verticalSpaceMedium,
+              verticalSpace(0.03.sh),
               TextFieldWidget(
                 textEditingController: viewModel.emailController,
-                width: 300,
+                width: 0.78.sw,
                 hintText: "Email",
                 prefixIcon: const Icon(Icons.email_outlined),
               ),
-              verticalSpaceMedium,
+              verticalSpace(0.03.sh),
               TextFieldWidget(
                 isObscure: true,
                 textEditingController: viewModel.passwordController,
-                width: 300,
+                width: 0.78.sw,
                 hintText: "Password",
                 prefixIcon: const Icon(Icons.lock_outline),
               ),
-              verticalSpaceMedium,
+              verticalSpace(0.03.sh),
               TextFieldWidget(
                 isObscure: true,
                 textEditingController: viewModel.confirmPasswordController,
-                width: 300,
+                width: 0.78.sw,
                 hintText: "Confirm Password",
                 prefixIcon: const Icon(Icons.lock_outline),
               ),
-              verticalSpaceMedium,
+              verticalSpace(0.03.sh),
               CustomTextButtonWidget(
                   textInButton: "Sign Up",
-                  onPressed: () {
-                    viewModel.createNewUser();
+                  onPressed: () async {
+                    await viewModel.createNewUser();
+                    if (context.mounted) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Center(
+                              child: Text(viewModel.signUpResponseMessage))));
+                    }
                   }),
-              verticalSpaceMedium,
+              verticalSpace(0.03.sh),
               const Text("Already a user?"),
               TextButton(
                   onPressed: () {
