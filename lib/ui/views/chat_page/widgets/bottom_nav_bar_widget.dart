691fbad487eff9490d5c9907cf2816769828137c
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-06 15:00:43 +0545

    feat: integrate message text field widget

--- lib/ui/views/chat_page/widgets/bottom_nav_bar_widget.dart
+++ lib/ui/views/chat_page/widgets/bottom_nav_bar_widget.dart
@@ -4,6 +4,7 @@ import 'package:stacked/stacked_annotations.dart';
 import 'package:whatsapp_stacked_architecture/ui/common/ui_helpers.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/chat_page/chat_page_viewmodel.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/chat_page/widgets/bottom_nav_bar_floating_button.dart';
+import 'package:whatsapp_stacked_architecture/ui/views/common_widgets/message_text_field_widget.dart';
 
 @FormView(fields: [FormTextField(name: 'messageInput')])
 
@@ -28,35 +29,9 @@ class BottomNavBar extends StatelessWidget {
         child: Row(
           children: [
             Expanded(
-              child: TextField(
-                controller: viewModel.messageInputController,
-                onTap: () {
-                  /// Updates the text message inserted in the message field
-                  /// and triggers an icon change.
-                  context.read<ChatPageViewModel>().setIcon();
-                },
-                decoration: InputDecoration(
-                  hintText: "Message",
-                  fillColor: kDefaultIconLightColor,
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(30)),
-
-                  /// Adds a prefix icon in front of the message text field.
-                  prefixIcon: const Icon(Icons.emoji_emotions),
-                  suffixIcon: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                          onPressed: () {}, icon: const Icon(Icons.attachment)),
-                      IconButton(
-                          onPressed: () {}, icon: const Icon(Icons.camera_alt)),
-                    ],
-                  ),
-                ),
-              ),
+              child: MessageTextFieldWidget(
+                  controller: viewModel.messageInputController,
+                  changeIconOnTap: context.read<ChatPageViewModel>().setIcon),
             ),
             horizontalSpaceSmall,
 
