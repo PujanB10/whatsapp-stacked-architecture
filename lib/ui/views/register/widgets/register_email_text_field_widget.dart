78026d5c17ee9c31558994f305b94800bf10fd0e
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-08 11:04:36 +0545

    feat: add comments in email text field widget

--- lib/ui/views/register/widgets/register_email_text_field_widget.dart
+++ lib/ui/views/register/widgets/register_email_text_field_widget.dart
@@ -3,8 +3,12 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/common_widgets/text_field_widget.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/register/register_viewmodel.dart';
 
+/// A widget for capturing the user's email during registration.
 class RegisterEmailTextFieldWidget extends StatelessWidget {
+  /// Reference to the view model for data control.
   final RegisterViewModel viewModel;
+
+  /// Creates a [RegisterEmailTextFieldWidget] to capture user's email.
   const RegisterEmailTextFieldWidget({
     required this.viewModel,
     super.key,
@@ -12,6 +16,7 @@ class RegisterEmailTextFieldWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Returns a text field with an email icon and set width.
     return TextFieldWidget(
       textEditingController: viewModel.emailController,
       width: 0.78.sw,
