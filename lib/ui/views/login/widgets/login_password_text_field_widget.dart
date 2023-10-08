c02104ab4bb56f214ba533839559f6a07ad4caa3
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-08 11:03:57 +0545

    feat: add comments in text field widget

--- lib/ui/views/login/widgets/login_password_text_field_widget.dart
+++ lib/ui/views/login/widgets/login_password_text_field_widget.dart
@@ -3,8 +3,12 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/common_widgets/text_field_widget.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/login/login_viewmodel.dart';
 
+/// A widget for capturing the user's password during login.
 class LoginPasswordTextFieldWidget extends StatelessWidget {
+  /// Reference to the view model for data control.
   final LoginViewModel viewModel;
+
+  /// Creates a [LoginPasswordTextFieldWidget] to capture user's password.
   const LoginPasswordTextFieldWidget({
     required this.viewModel,
     super.key,
@@ -12,6 +16,7 @@ class LoginPasswordTextFieldWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Returns an obscured text field with a lock icon and set width.
     return TextFieldWidget(
       isObscure: true,
       textEditingController: viewModel.passwordController,
