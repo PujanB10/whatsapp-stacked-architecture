c4e831813060e351c0138c12fa748ee83a03414f
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-08 12:17:41 +0545

    feat: add comments in fetch user service

--- lib/services/fetch_user_list_service.dart
+++ lib/services/fetch_user_list_service.dart
@@ -3,11 +3,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:whatsapp_stacked_architecture/datamodels/user_model.dart';
 
+/// A service class to fetch the users and their details for the home page.
 class FetchUserListService {
   final db = FirebaseFirestore.instance;
-  List<Users> listOfUsers = List.empty(growable: true);
 
   Stream<List<Users>> fetchUserList() {
+    /// Fetches the user details from the Firestore database and returns after
+    /// converting the details that is in JSON into an instance of User model.
     return db
         .collection("users")
         .snapshots()
