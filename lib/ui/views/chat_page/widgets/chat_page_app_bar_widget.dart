7524214fe97a636c8d5c724f5469ffc4213758f4
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-08 11:03:30 +0545

    feat: add comments in chat page app bar widget

--- lib/ui/views/chat_page/widgets/chat_page_app_bar_widget.dart
+++ lib/ui/views/chat_page/widgets/chat_page_app_bar_widget.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/chat_page/chat_page_viewmodel.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/common_widgets/app_bar_title_text_widget.dart';
 
+/// An appbar widget that resides on top of the chat page.
 class ChatPageAppBarWidget extends StatelessWidget
     implements PreferredSizeWidget {
+  /// Creates a [ChatPageAppBarWidget].
+  ///
+  /// [imageUrl],[username] and [viewModel] must not be null.
   const ChatPageAppBarWidget(
       {super.key,
+
+      /// [imageUrl] is the url of the image of user whose chat message has been opened.
       required this.imageUrl,
+
+      /// [username] is the username of user whose chat message has been opened.
       required this.username,
+
+      /// [viewModel] is the instance of the view model of chat page.
       required this.viewModel});
 
   @override
