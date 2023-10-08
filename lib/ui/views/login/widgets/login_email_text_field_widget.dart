ea0ca1001e3bc1f7a16435ff67d16821fbdcbb41
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-08 11:03:41 +0545

    feat: add comments in login email text field

--- lib/ui/views/login/widgets/login_email_text_field_widget.dart
+++ lib/ui/views/login/widgets/login_email_text_field_widget.dart
@@ -3,8 +3,12 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/common_widgets/text_field_widget.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/login/login_viewmodel.dart';
 
+/// A widget for capturing the user's email during login.
 class LoginEmailTextFieldWidget extends StatelessWidget {
+  /// Reference to the view model for data control.
   final LoginViewModel viewModel;
+
+  /// Creates a [LoginEmailTextFieldWidget] to capture user's email.
   const LoginEmailTextFieldWidget({
     required this.viewModel,
     super.key,
@@ -12,6 +16,7 @@ class LoginEmailTextFieldWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Returns a text field with an email icon and set width.
     return TextFieldWidget(
       textEditingController: viewModel.emailController,
       width: 0.78.sw,
