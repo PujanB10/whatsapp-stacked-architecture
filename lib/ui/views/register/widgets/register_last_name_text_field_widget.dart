5ee801162ccb68516703afdde908ae51fe26c079
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-08 11:05:04 +0545

    feat: add comments in last name text field widget

--- lib/ui/views/register/widgets/register_last_name_text_field_widget.dart
+++ lib/ui/views/register/widgets/register_last_name_text_field_widget.dart
@@ -3,8 +3,12 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/common_widgets/text_field_widget.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/register/register_viewmodel.dart';
 
+/// A widget for capturing the user's last name during registration.
 class RegisterLastNameTextFieldWidget extends StatelessWidget {
+  /// Reference to the view model for data control.
   final RegisterViewModel viewModel;
+
+  /// Creates a [RegisterLastNameTextFieldWidget] to capture user's last name.
   const RegisterLastNameTextFieldWidget({
     required this.viewModel,
     super.key,
@@ -12,6 +16,7 @@ class RegisterLastNameTextFieldWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Returns a text field with a person icon and set width.
     return TextFieldWidget(
       textEditingController: viewModel.lastNameController,
       width: 0.39.sw,
