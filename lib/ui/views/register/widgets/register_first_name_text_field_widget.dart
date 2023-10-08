eb3a3ec87094a975742341b97052a38daeba5e67
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-08 11:04:50 +0545

    feat: add comments in first name text field widget

--- lib/ui/views/register/widgets/register_first_name_text_field_widget.dart
+++ lib/ui/views/register/widgets/register_first_name_text_field_widget.dart
@@ -3,8 +3,12 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/common_widgets/text_field_widget.dart';
 import 'package:whatsapp_stacked_architecture/ui/views/register/register_viewmodel.dart';
 
+/// A widget for capturing the user's first name during registration.
 class RegisterFirstNameTextFieldWidget extends StatelessWidget {
+  /// Reference to the view model for data control.
   final RegisterViewModel viewModel;
+
+  /// Creates a [RegisterFirstNameTextFieldWidget] to capture user's first name.
   const RegisterFirstNameTextFieldWidget({
     required this.viewModel,
     super.key,
@@ -12,6 +16,7 @@ class RegisterFirstNameTextFieldWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Returns a text field with a person icon and set width.
     return TextFieldWidget(
       textEditingController: viewModel.firstNameController,
       width: 0.39.sw,
