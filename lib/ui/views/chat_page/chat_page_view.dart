e2b02850482055bc6791329eb38d1503717c89ac
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-09 15:18:06 +0545

    feat: add comments in chat screen

--- lib/ui/views/chat_page/chat_page_view.dart
+++ lib/ui/views/chat_page/chat_page_view.dart
@@ -6,7 +6,9 @@ import 'package:whatsapp_stacked_architecture/ui/views/chat_page/widgets/chat_pa
 import 'package:whatsapp_stacked_architecture/ui/views/chat_page/widgets/message_list_view_widget.dart';
 import 'chat_page_viewmodel.dart';
 
+/// A view for displaying a chat conversation with a specific user.
 class ChatPageView extends StackedView<ChatPageViewModel> {
+  /// Creates a [ChatPageView] with the given [username], [imageUrl], and [receiverUserId].
   const ChatPageView(
       {Key? key,
       required this.username,
@@ -14,8 +16,13 @@ class ChatPageView extends StackedView<ChatPageViewModel> {
       required this.receiverUserId})
       : super(key: key);
 
+  /// The username of the chat recipient.
   final String username;
+
+  /// The URL of the chat recipient's profile image.
   final String imageUrl;
+
+  /// The ID of the user with whom the chat screen is.
   final String receiverUserId;
 
   @override
