69edffd151724f0f357cc017c73c897f70793de1
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-10-17 15:57:07 +0545

    feat: unit test for chat page viewmodel

--- test/viewmodels/chat_page_viewmodel_test.dart
+++ test/viewmodels/chat_page_viewmodel_test.dart
@@ -1,11 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:whatsapp_stacked_architecture/app/app.locator.dart';
-
+import 'package:whatsapp_stacked_architecture/ui/views/chat_page/chat_page_viewmodel.dart';
 import '../helpers/test_helpers.dart';
+
+class MockFirebase extends Mock implements Firebase {}
 
 void main() {
   group('ChatPageViewModel Tests -', () {
-    setUp(() => registerServices());
+    setUp(() {
+      registerServices();
+    });
     tearDown(() => locator.reset());
+
+    test("Must return chat id separated by hyphen in alphabetical order", () {
+      ChatPageViewModel model = ChatPageViewModel();
+      String chatId =
+          model.getChatId("jk3jj4b3b4jh3b4", "3jk432kj4nj3k4buy34b");
+      expect(chatId, "jk3jj4b3b4jh3b4-3jk432kj4nj3k4buy34b");
+    });
   });
 }
